58e356bd065547a5c2a83a4e9060a72a9d5d2e72
barvaliyavishal <barvaliyavishal>
2023-03-24 23:27:58 +0000

    SAS

--- Setup/2.access_adls_using_sas_token.py
+++ Setup/2.access_adls_using_sas_token.py
@@ -7,17 +7,17 @@
 
 # COMMAND ----------
 
-spark.conf.set("fs.azure.account.auth.type.formula1dl.dfs.core.windows.net", "SAS")
-spark.conf.set("fs.azure.sas.token.provider.type.formula1dl.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
-spark.conf.set("fs.azure.sas.fixed.token.formula1dl.dfs.core.windows.net", "")
+spark.conf.set("fs.azure.account.auth.type.vishalformula1dl.dfs.core.windows.net", "SAS")
+spark.conf.set("fs.azure.sas.token.provider.type.vishalformula1dl.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
+spark.conf.set("fs.azure.sas.fixed.token.vishalformula1dl.dfs.core.windows.net", "sp=racwdlmeop&st=2023-03-24T23:26:30Z&se=2023-03-25T07:26:30Z&spr=https&sv=2021-12-02&sr=c&sig=5ZWiitJO41OjTfAbqqrXPXXyRiGb1%2FT6M0b1THc4fEc%3D")
 
 # COMMAND ----------
 
-display(dbutils.fs.ls("abfss://demo@formula1dl.dfs.core.windows.net"))
+display(dbutils.fs.ls("abfss://demo@vishalformula1dl.dfs.core.windows.net"))
 
 # COMMAND ----------
 
-display(spark.read.csv("abfss://demo@formula1dl.dfs.core.windows.net/circuits.csv"))
+display(spark.read.csv("abfss://demo@vishalformula1dl.dfs.core.windows.net/circuites.csv"))
 
 # COMMAND ----------
 
