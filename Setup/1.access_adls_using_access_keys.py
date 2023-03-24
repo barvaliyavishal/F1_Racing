e5347f91ce4ca77d0a5a93b3a369f083b70c9a17
barvaliyavishal <barvaliyavishal>
2023-03-24 23:26:10 +0000

    Access Tokens

---
+++ Setup/1.access_adls_using_access_keys.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #### Access Azure Data Lake using access keys
+# MAGIC 1. Set the spark config fs.azure.account.key
+# MAGIC 1. List files from demo container
+# MAGIC 1. Read data from circuits.csv file
+
+# COMMAND ----------
+
+spark.conf.set(
+    "fs.azure.account.key.vishalformula1dl.dfs.core.windows.net",
+    "ck1FWENS3epHiHtZ1gnAyW09hgADYZZ7PNX9UMoM3OvvYXLF7DR7wsoeRomrqbTqdogI88dRvUiA+AStVbQ+tw==")
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("abfss://demo@vishalformula1dl.dfs.core.windows.net"))
+
+# COMMAND ----------
+
+display(spark.read.csv("abfss://demo@vishalformula1dl.dfs.core.windows.net/circuites.csv"))
+
+# COMMAND ----------
+
+
